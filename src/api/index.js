32f8adb3eb25b55b46fbd8a6979140188564f4af
Grzegorz Lechowski <Grzegorz.Lechowski@alior.pl>
2019-01-18 01:58:27 +0100

    added traslates for /pl/ and /eng/ language.

--- src/api/index.js
+++ src/api/index.js
@@ -12,72 +12,169 @@ export default ({
 }) => {
 	let api = Router();
 
+
+	/*
+	{
+		"lang": {
+			"extension": {
+				"feature": {
+					"intention": {
+						[...]
+					}
+				} 
+			},
+			"controls": {
+				[...]
+			}
+		}
+	}
+	*/
+
 	let messages = new Object({
-		authentication: {
-			form: {
-				steps: {
-					one: {
-						title: 'Base Informations',
+		en: {
+			main: {
+				header: {
+					title: "Application Managment System",
+					subtitle: "Powered by {{ value }}",
+					language: {
+						select: {
+							label: "Change country",
+							placeholder: "Select...",
+						}
+					}
+				},
+				footer: {
+					text: "AMS | Alior Bank"
+				}
+			},
+			test: "translation tests",
+			development: {
+				registerStepper: {
+					primaryData: {
+						title: "Client Contact Data"
 					},
-					two: {
-						title: 'Personal Informations',
+					extendedData: {
+						title: "Client Personal Data"
+					},
+					additionalData: {
+						title: "Client Additional Data"
 					}
 				}
 			},
-			email: {
-				label: "E-mail",
-				placeholder: 'Wpisz imię...'
-			},
-			name: {
-				label: "Nazwa",
-				placeholder: 'Wpisz nazwę...'
-			},
-			password: {
-				label: "Hasło",
-				placeholder: 'Wpisz hasło...',
-				tooltip: 'Przykładowy tooltip'
+			formControls: {
+				default: {
 
+				},
+				email: {
+					label: "E-mail",
+					placeholder: 'Enter email...'
+				},
+				name: {
+					label: "Name",
+					placeholder: 'Enter name...'
+				},
+				password: {
+					label: "Password",
+					placeholder: 'Enter passpharase...',
+					tooltip: 'Example tooltip'
+	
+				},
+				date: {
+					label: "Date",
+					placeholder: 'Pick date...'
+				},
+				displayName: {
+					label: "Display Name",
+					placeholder: 'Wpisz widoczną nazwę...',
+					extPlaceholder: {value: 'Wpisz widoczną nazwę...'}
+				},
+				adult: {
+					label: 'Yes, I am over 21 years old',
+				},
+				agreement: {
+					label: 'Agreement',
+				},
+				firstName: {
+					label: 'First Name',
+					placeholder: 'Enter first name...'
+				}
+			}
+		},
+		pl: {
+			main: {
+				header: {
+					title: "System zarządzania aplikacjami",
+					subtitle: "Napędzany przez {{ value }}",
+					language: {
+						select: {
+							label: "Zmień kraj",
+							placeholder: "Wybierz...",
+						}
+					}
+				},
+				footer: {
+					text: "AMS | Alior Bank"
+				}
 			},
-			displayName: {
-				label: "Widoczna nazwa",
-				placeholder: 'Wpisz widoczną nazwę...'
+			test: "testy tłumaczeń",
+			development: {
+				registerStepper: {
+					primaryData: {
+						title: "Dane Kontaktowe"
+					},
+					extendedData: {
+						title: "Dane Personalne"
+					},
+					additionalData: {
+						title: "Dane Uzupełniające"
+					}
+				}
 			},
-			adult: {
-				label: 'Oświadczenie',
-			},
-			agreement: {
-				label: 'Oświadczenie',
-			},
-			firstName: {
-				label: 'Imię',
-				placeholder: 'Wpisz imię...'
+			formControls: {
+				default: {
+	
+				},
+				email: {
+					label: "E-mail",
+					placeholder: 'Wpisz email...'
+				},
+				name: {
+					label: "Nazwa",
+					placeholder: 'Wpisz name...'
+				},
+				password: {
+					label: "Hasło",
+					placeholder: 'Wpisz hasło...',
+					tooltip: 'Przykładowy tooltip'
+	
+				},
+				date: {
+					label: "Data",
+					placeholder: 'Wybierz datę...'
+				},
+				displayName: {
+					label: "Widoczna Nazwa",
+					placeholder: 'Wpisz widoczną nazwę...'
+				},
+				adult: {
+					label: 'Tak, ukoczyłem 18 lat',
+				},
+				agreement: {
+					label: 'Oświadczenie',
+				},
+				firstName: {
+					label: 'Imię',
+					placeholder: 'Wpisz imię...'
+				},
+				lastName: {
+					label: 'Nazwisko',
+					placeholder: 'Wpisz nazwisko...'
+				}
 			}
 		}
 	});
 
 	let users = new Object([
-		{
-			name: 'uland',
-			displayName: 'Uland Nimblehoof',
-			email: 'uland@gmail.com',
-			adult: true
-		},
-		{
-			name: 'okyl',
-			displayName: 'Okyl Bjornsen',
-			email: 'okyl@gmail.com',
-			adult: false
-		},
-		{
-			name: 'malcom',
-			displayName: 'Malcom Freeman',
-			email: 'uland@gmail.com',
-			adult: true
-
-		}
-	]);
-
-	let newUsers = new Object([
 		{
 			id: 1,
 			name: 'uland',
@@ -114,22 +211,28 @@ export default ({
 		});
 	});
 
-	api.get('/users', (req, res) => res.json(users));
 	api.get('/messages', (req, res) => {
 		res.json(JSON.stringify(messages));
 	});
 
-	api.get('reduxList', (req, res) => {
-		res.send('Working')
+	api.get('/messages/pl', (req, res) => {
+		res.json(JSON.stringify(messages.pl));
 	});
+
+	api.get('/messages/en', (req, res) => {
+		res.json(JSON.stringify(messages.en));
+	});
+
 	api.get('/getUsers', (req, res) => {
-		console.log(JSON.stringify(newUsers));
-		res.json(JSON.stringify(newUsers))
+		console.log(users);
+		res.json(JSON.stringify(users));
 	});
 
 	api.post('/updateUser', (req, res) => {
-		console.log(req.body);
-		
+		let updatedUser = users.find(user => user.id === req.body.id);
+		updatedUser.adult = req.body.value;
+		console.log(req.body, '\r updated: ', updatedUser);
+		res.json(JSON.stringify(updatedUser));
 	});
 
 	return api;
